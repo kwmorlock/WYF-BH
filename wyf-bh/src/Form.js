d8eb2aeef02dc0f240c1e2ad97007444d3caf728
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-03-27 10:11:04 -0400

    second

--- wyf-bh/src/Form.js
+++ wyf-bh/src/Form.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 import axios from "axios";
 import * as yup from "yup";
+
 const formSchema = yup.object().shape({
     name: yup.string().required("please input a name").min(2, "name must be more than 2 characters"),
     email: yup.string().email("must be a valid email").required("email is a required field"),
@@ -37,4 +38,53 @@ export default function Form() {
             setButton(!valid);
         });
     }, [formState]);
+    const formSubmit = e => {
+        e.preventDefault();
+        axios
+            .post("https://reqres.in/api/orders", formState)
+            .then(res => {
+                setPost(res.data); 
+                setFormState({
+                    name: "",
+                    email: "",
+                    address: "",
+                    flavor: "",
+                    size: "",
+                    terms: ""
+                });
+            })
+            .catch(err => console.log("something went wrong when submitting your form", err.response));
+    };
+    const validateChange = e => {
+        yup 
+            .reach(formSchema, e.target.name)
+            .validate(e.target.name === "terms" ? e.target.checked : e.target.value)
+            .then(valid => {
+                setErr({
+                    ...err,
+                    [e.target.name]: ""
+                });
+            })
+            .catch(err => {
+                setErr({
+                    ...err,
+                    [e.target.name]: err.err[0]
+                });
+            });
+    };
+    const inputChange = e => {
+        e.persist(); // constantly checking to see what the user is typing in and checking if its valid
+        const newFormData = {
+            ...formState,
+            [e.target.name]:
+            e.target.type  === "checkbox" ? e.target.checked : e.target.value
+        };
+        validateChange(e);
+        setFormState(newFormData);
+    };
+    return (
+        <form>
+            <h1>If you could be any flavor...what flavor would you be?</h1>
+        </form>
+    )
 }
