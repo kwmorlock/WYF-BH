8f5eedbbaf76a94f6856649af3b66d2099bb15c9
Chelsea Wetzel <chelswetz@gmail.com>
2020-03-27 00:53:29 -0400

    form

--- wyf-bh/src/Components/Home.js
+++ wyf-bh/src/Components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
             <h2>build a t-shirt for any flavor that you identify as!</h2>
             <img className="t-shirt" src="https://cdn2.bigcommerce.com/server2800/677b8/products/26200/images/114756/11402f__96790.1490634559.1100.1100.jpg?c=2" alt="corn on the cob t-shirt" />
             <h3>click here to order your t-shirt! 👇</h3>
-            <Link className="flexin" to={"/order"}>
+            <Link className="flexin" to={"/form"}>
                 <div className="order-here">Get Orderin'</div>
             </Link>
         </div>
